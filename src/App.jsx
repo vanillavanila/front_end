dd5376598540e22c996296bb7dab4fb03f8fe72f
vanillavanila <indraramadani411@gmail.com>
2026-05-27 19:31:04 +0800

    fix:bug

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <div className="app-wrapper">
       <Router>
         
-        <main style={{ overflowX:'hidden', marginRight:'1em' }}>
+        <main style={{ overflowX:'hidden', ' }}>
         <Navbar />
           <Routes>
             {/* Halaman Home */}
