97e79df7eaf0c2aab3e4aadb7414fef8db4562d7
vanillavanila <indraramadani411@gmail.com>
2026-05-28 20:00:48 +0800

    fix: bug

--- src/components/Extracurriculars.jsx
+++ src/components/Extracurriculars.jsx
@@ -42,8 +42,7 @@ const Extracurriculars = () => {
   }, []);
 
   return (
-    <section id="ekskul" className="section bg-main text-center group" ref={sectionRef}>
-      <div className="jp-matrix  container text-center">
+    <section id="ekskul" className="jp-matrix section bg-main text-center group" ref={sectionRef}>
          <span>+</span><span>−</span><span>×</span><span>÷</span><span>=</span
   ><span>≠</span><span>≈</span><span>∞</span><span>√</span><span>∑</span
   ><span>∏</span><span>∫</span><span>∂</span><span>∆</span><span>π</span
@@ -308,6 +307,7 @@ const Extracurriculars = () => {
   ><span>ℂ</span><span>|</span><span>∥</span><span>∠</span><span>⊥</span
   ><span>≅</span><span>∝</span><span>∴</span><span>∵</span><span>⊕</span
   ><span>⊗</span><span>⊥</span><span>⊢</span><span>⊨</span><span>∇</span>
+      <div className="container text-center">
         <h2 className="h2 mb-4">Ekstrakurikuler</h2>
         <p className="text-muted mb-12 max-w-2xl mx-auto">
           Kembangkan bakat dan minatmu melalui berbagai kegiatan ekstrakurikuler pilihan yang didukung oleh fasilitas memadai dan pembina profesional.
